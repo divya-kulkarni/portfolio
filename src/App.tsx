eadf98b83b384878ac62ffea7d718dee746e4885
Divya Kulkarni <divyak98@gmail.com>
2024-02-02 01:14:43 +0900

    Added skills section

--- src/App.tsx
+++ src/App.tsx
@@ -3,12 +3,14 @@ import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css'
 import { NavBar } from './components/navbar';
 import { Hero } from './components/hero';
+import { Skills } from './components/skills';
 
 function App() {
   return (
     <div className="App">
       <NavBar />
-      < Hero />
+      <Hero />
+      <Skills />
     </div>
   );
 }
---
+++ src/components/skills.tsx
@@ -0,0 +1,34 @@
+import '../assets/styling/skills.css';
+import navIcon3 from '../assets/images/nav-icon3.svg';
+
+export const Skills = () => {
+    return (
+        <div className="skills">
+            <h2>Skills</h2>
+            <div className="skills-container">
+                <div className="frontend-skills">
+                    <h4>Frontend Skills</h4>
+                    <span><img src={navIcon3} alt=""></img></span>
+                    <span><img src={navIcon3} alt=""></img></span>
+                    <span><img src={navIcon3} alt=""></img></span>
+                    <span><img src={navIcon3} alt=""></img></span>
+                    <span><img src={navIcon3} alt=""></img></span>
+                </div>
+                <div className="backend-skills">
+                    <h4>Backend Skills</h4>
+                    <span><img src={navIcon3} alt=""></img></span>
+                    <span><img src={navIcon3} alt=""></img></span>
+                    <span><img src={navIcon3} alt=""></img></span>
+                    <span><img src={navIcon3} alt=""></img></span>
+                    <span><img src={navIcon3} alt=""></img></span>
+                </div>
+                <div className="other-skills">
+                    <h4>Other Skills</h4>
+                    <span><img src={navIcon3} alt=""></img></span>
+                    <span><img src={navIcon3} alt=""></img></span>
+                    <span><img src={navIcon3} alt=""></img></span>
+                </div>
+            </div>
+        </div>
+    );
+};
