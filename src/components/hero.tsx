86efc032de0ac912852e2767c1e747f9585b2c5c
Divya Kulkarni <divyak98@gmail.com>
2024-02-02 14:18:16 +0900

    Made buttons responsive

--- src/components/hero.tsx
+++ src/components/hero.tsx
@@ -1,10 +1,7 @@
 import { Container, Row, Col } from "react-bootstrap";
 import '../assets/styling/hero.css';
 import heroImg from '../assets/images/woman.jpg';
-import resume from '../assets/resume/resume.pdf';
-import navIcon1 from '../assets/images/nav-icon1.svg';
-import navIcon2 from '../assets/images/nav-icon2.svg';
-import navIcon3 from '../assets/images/nav-icon3.svg';
+// import resume from '../assets/resume/resume.pdf';
 
 export const Hero = () => {
     return (
@@ -14,15 +11,13 @@ export const Hero = () => {
                     <Col xs={12} md={6} xl={7}>
                         <span className="hero-name">{`<b>Hi, I'm Divya</b>`}</span>
                         <h1>{`Front-End Web Developer`}</h1>
-                        <p>Lorem ipsum It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters, as opposed to using Content here, content here, making it look like readable English.</p>
-                        <a href={resume} rel="noopener noreferrer" target="_blank">
-                            <button><span>Resume</span></button>
-                        </a>
-                        <button className="contact-button" onClick={() => console.log('Ssup?')}><span>Say Hi 👋</span></button>
-                        <div className="social">
-                            <a href="#linkedin"><img src={navIcon1} alt=""></img></a>
-                            <a href="#github"><img src={navIcon2} alt=""></img></a>
-                            <a href="#email"><img src={navIcon3} alt=""></img></a>
+                        <p>Lorem ipsum It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout.</p>
+                        <div className="hero-action">
+                            {/* <a href={resume} rel="noopener noreferrer" target="_blank">
+                                <button>Resume</button>
+                            </a>*/}
+                            <a href="something" className="action-btn">Resume</a>
+                            <a href="something" className="action-btn">Say Hi 👋</a> 
                         </div>
                     </Col>
                     <Col xs={12} md={6} xl={5}>
