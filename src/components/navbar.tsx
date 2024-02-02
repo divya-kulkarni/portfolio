68956eb8c9814b23aae028f87303fdcf15236776
Divya Kulkarni <divyak98@gmail.com>
2024-02-02 18:49:37 +0900

    Added scroll functionality to navbar

--- src/components/navbar.tsx
+++ src/components/navbar.tsx
@@ -3,19 +3,25 @@ import '../assets/styling/navbar.css';
 
 
 export const NavBar = () => {
+  const handleClickScroll = (sectionId:string) => {
+    const element = document.getElementById(sectionId);
+    if (element) {
+      element.scrollIntoView({ behavior: 'smooth' });
+    }
+  };
   return (
     <Navbar expand="lg" className="bg-body-tertiary" sticky="top">
       <Container>
-        <Navbar.Brand href="#home">DivyaDev</Navbar.Brand>
+        <Navbar.Brand href="#home" onClick={(e: React.MouseEvent<HTMLElement>) => handleClickScroll('hero')}>DivyaDev</Navbar.Brand>
         <Navbar.Toggle aria-controls="basic-navbar-nav">
             <span className="navbar-toggler-icon"></span>
         </Navbar.Toggle>
         <Navbar.Collapse id="basic-navbar-nav">
           <Nav className="me-auto">
-            <Nav.Link href="#home">Home</Nav.Link>
-            <Nav.Link href="#skills">Skills</Nav.Link>
-            <Nav.Link href="#about">About</Nav.Link>
-            <Nav.Link href="#projects">Projects</Nav.Link>
+            <Nav.Link href="#home" onClick={(e: React.MouseEvent<HTMLElement>) => handleClickScroll('hero')}>Home</Nav.Link>
+            <Nav.Link href="#skills" onClick={(e: React.MouseEvent<HTMLElement>) => handleClickScroll('skills')}>Skills</Nav.Link>
+            <Nav.Link href="#projects" onClick={(e: React.MouseEvent<HTMLElement>) => handleClickScroll('projects')}>Projects</Nav.Link>
+            <Nav.Link href="#contact">Contact</Nav.Link>
           </Nav>
         </Navbar.Collapse>
       </Container>
