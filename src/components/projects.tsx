960e8181e79f8c66923393b73517c8a73253df0c
Divya Kulkarni <divyak98@gmail.com>
2024-02-10 23:20:46 +0900

    Added assets in project component

--- src/components/projects.tsx
+++ src/components/projects.tsx
@@ -1,10 +1,10 @@
-/* eslint-disable react/jsx-no-duplicate-props */
-import Card from "react-bootstrap/Card";
 import Col from "react-bootstrap/Col";
 import Row from "react-bootstrap/Row";
 import "../assets/styling/projects.css";
 import tns from "../assets/images/screenshots/tns.png";
 import portfolio from "../assets/images/screenshots/portfolio.png";
+import plants from "../assets/images/screenshots/plants.png";
+import portfolio2 from "../assets/images/screenshots/portfolio2.jpg";
 
 export const Projects = () => {
   return (
@@ -16,6 +16,12 @@ export const Projects = () => {
         </Col>
         <Col xs={12} sm={9} md={6} className="mb-2">
           <img src={portfolio} alt="portfolio" className="img-fluid" />
+        </Col>
+        <Col xs={12} sm={9} md={6} className="mb-2">
+          <img src={plants} alt="portfolio" className="img-fluid" />
+        </Col>
+        <Col xs={12} sm={9} md={6} className="mb-2">
+          <img src={portfolio2} alt="portfolio" className="img-fluid" />
         </Col>
       </Row>
     </div>
