300c923a8fb654e991a8da2aee40877c10e25f2f
Divya Kulkarni <divyak98@gmail.com>
2024-03-12 22:06:40 +0900

    removed cursor

--- src/App.tsx
+++ src/App.tsx
@@ -7,10 +7,34 @@ import { Contact } from "./components/contact";
 import { NotFound } from "./components/404";
 import { TnsCaseStudy } from "./components/TnsCaseStudy";
 import { PortfolioCaseStudy } from "./components/PortfolioCaseStudy";
+// import AnimatedCursor from "react-animated-cursor";
 
 function App() {
   return (
     <div className="App">
+      {/* <AnimatedCursor
+        innerSize={8}
+        outerSize={35}
+        color="233, 223, 206"
+        outerAlpha={0.5}
+        outerStyle={{
+          border: "1px solid var(--cursor-color)",
+          backgroundColor: "none",
+        }}
+        innerScale={1}
+        outerScale={3}
+        clickables={[
+          "a",
+          'input[type="text"]',
+          'input[type="email"]',
+          'input[type="number"]',
+          'input[type="submit"]',
+          'input[type="image"]',
+          "img",
+          "button",
+          ".link",
+        ]}
+      /> */}
       <Routes>
         <Route path="/" element={<Portfolio />} />
         <Route path="/about" element={<About />} />
