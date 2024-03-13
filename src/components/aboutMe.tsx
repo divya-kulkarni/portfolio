f94ccae76d6882869e4781beaf5720c44194e1cd
Divya Kulkarni <divyak98@gmail.com>
2024-03-13 22:39:49 +0900

    Updated about page

--- src/components/aboutMe.tsx
+++ src/components/aboutMe.tsx
@@ -3,6 +3,8 @@ import { NavBar } from "./navbar";
 import { Col, Row } from "react-bootstrap";
 import "../styling/about.css";
 import myPhoto from "../assets/images/divya.png";
+import heart from "../assets/images/about/heart.jpg";
+import film from "../assets/images/about/film2.jpg";
 
 export const About = () => {
   const getNextPage = (path: string) => {
@@ -15,7 +17,21 @@ export const About = () => {
       <div className="about">
         <Row>
           <Col sm={12} md={6} className="carousel">
-            <img src={myPhoto} alt="Divya Kulkarni" className="my-photo" />
+            <Row className="grid">
+              <img src={myPhoto} alt="Divya Kulkarni" className="my-photo" />
+            </Row>
+            <Row className="grid">
+              <Col sm={6}>
+                <img
+                  src={heart}
+                  alt="A pen sketch of a heart"
+                  className="my-photo"
+                />
+              </Col>
+              <Col sm={6}>
+                <img src={film} alt="A boat on a beach" className="my-photo" />
+              </Col>
+            </Row>
           </Col>
           <Col sm={12} md={6} className="about-text">
             <h2>Who is Divya?</h2>
@@ -35,7 +51,9 @@ export const About = () => {
             <p>
               And of course, my love affair with Mt. Fuji 🗻 continues to
               inspire me—whether it's incorporating its beauty into my artwork
-              or simply gazing at its majestic silhouette on the horizon.
+              or simply gazing at its majestic silhouette on the horizon. See
+              more on my <a href="https://vsco.co/dkbose98/gallery">VSCO</a>{" "}
+              page. 📸
               <br />
               <br />
               Interested in my work? Explore my portfolio and discover how I
@@ -43,8 +61,7 @@ export const About = () => {
               <br />
               <br />
               Feel free to reach out if you'd like to discuss tech, art, or the
-              perfect spots for admiring Mount Fuji. Check out my{" "}
-              <a href="https://vsco.co/dkbose98/gallery">VSCO</a> page. 📸
+              perfect spots for admiring Mount Fuji.
             </p>
             <div className="hero-action">
               <a
